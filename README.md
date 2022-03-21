8b3361678b7d93da03d50c35f4240d40007e85cd
Mohannad Otaibi <mohannad.otaibi@gmail.com>
2022-03-21 21:59:56 +0300

    initial init

---
+++ README.md
@@ -0,0 +1,40 @@
+# js_nuxt_6d.com.sa
+
+## Build Setup
+
+```bash
+# install dependencies
+$ npm install
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+
+# build for production and launch server
+$ npm run build
+$ npm run start
+
+# generate static project
+$ npm run generate
+```
+
+## Used Libraries
+
+```bash
+# Bootstrap and Bootstrap Icons
+# $ npm i --save bootstrap-icons bootstrap @popperjs/core
+# not used in this repo, we are taking tailwind as an experiment
+
+# SASS and SASS-Loader
+$ npm install  save-dev sass sass-loader@10.1.1
+```
+
+## Targets
+
+- [ ] 90 Above Page Speed
+- [ ] multi-lingual mode (i18n)
+- [ ] light/dark-mode css
+- [ ] bi-directional design elements
+- [ ] semantic html
+- [ ] meta social elements
+- [ ] night/dark favicon
+- [ ] tailwindy
---
+++ nuxt.config.js
@@ -0,0 +1,89 @@
+export default {
+  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  ssr: false,
+
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: '6 degrees | الدرجات الست',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: 'Where your technological future, comes today. The limits to technology uses is beyond imagination, digitize your life with us' },
+        { name: 'format-detection', content: 'telephone=no' },
+        { name: 'author', content: 'Six Degrees Technologies' },
+        { name: 'author', type: 'text/plain', href: '/humans.txt' },
+        { name: 'keywords', content: '' },
+        { name: 'canonical', content: 'https://www.6degrees.com.sa/' },
+  
+        { name: 'og:type', content: 'website' },
+        { name: 'og:title', content: '6 degrees | الدرجات الست' },
+        { name: 'og:description', content: 'Where your technological future, comes today. The limits to technology uses is beyond imagination, digitize your life with us' },
+        { name: 'og:image', content: 'https://www.6degrees.com.sa/social_icon.png' },
+        { name: 'og:url', content: 'https://www.6degrees.com.sa' },
+        { name: 'og:site_name', content: '6 degrees | الدرجات الست' },
+  
+        { name: 'twitter:card', content: 'summary_large_image' },
+        { name: 'twitter:title', content: '6 degrees | الدرجات الست' },
+        { name: 'twitter:description', content: 'Where your technological future, comes today. The limits to technology uses is beyond imagination, digitize your life with us' },
+        { name: 'twitter:image', content: 'https://www.6degrees.com.sa/social_icon.png' },
+        { name: 'twitter:url', content: 'https://www.6degrees.com.sa' },
+        { name: 'twitter:site', content: '@6degz' },
+        { name: 'twitter:creator', content: '@6degz' },
+      ],
+      link: [
+        { rel: 'icon', href: '/favicon.ico' },
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'shortcut icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'icon', type: 'image/svg+xml', href: '/logo.svg' },
+        { rel: 'icon', type: 'image/png', href: '/logo.png' },
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [ '@/assets/css/styles.scss'
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/tailwindcss
+    '@nuxtjs/tailwindcss',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    // https://go.nuxtjs.dev/content
+    '@nuxt/content',
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+    baseURL: '/',
+  },
+
+  // Content module configuration: https://go.nuxtjs.dev/config-content
+  content: {},
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  },
+  server: {
+    host: 'localhost',
+    port: '15152',
+}
+}
