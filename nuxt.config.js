5b9b235a1351286ee71781883458838caa8bf29e
Mohannad Otaibi <mohannad.otaibi@gmail.com>
2022-06-01 15:44:28 +0300

    ..

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,6 +15,10 @@ export default {
 				name: 'viewport',
 				content: 'width=device-width, initial-scale=1'
 			},
+            {
+				name: 'theme-color',
+				content: '#004Dff'
+			},
 			{
 				hid: 'description',
 				name: 'description',
